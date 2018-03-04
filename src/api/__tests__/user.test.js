b59751fc27d89e83c49f5e05d539c5ace8a96f75
Hstarorg <hm910705@163.com>
2018-03-04 22:10:48 +0800

    Change update to test case file

--- src/api/__tests__/user.test.js
+++ src/api/__tests__/user.test.js
@@ -1,50 +1,52 @@
-jest.mock("../http");
-import http from "../http";
+jest.mock('../http');
+
 import initUserData from '../../service/someNeedTest';
+import http from '../http';
+
+const util = require('../../util');
 
 // api mock test
-describe("user api test", () => {
-  const url = "xxx.yyy.com/user";
-  it("user GET should be 200", async () => {
+describe('user api test', () => {
+  const url = 'xxx.yyy.com/user';
+  it('user GET should be 200', async () => {
     const result = await http({
       url,
-      method : "get",
-      statusCode : 200
+      method: 'get',
+      statusCode: 200
     });
-    expect(result.data.username).toBe("Kodo");
+    expect(result.data.username).toBe('Kodo');
   });
-  
-  it("user POST should be 200", async () => {
+
+  it('user POST should be 200', async () => {
     const result = await http({
       url,
-      method : "post",
-      statusCode : 200
+      method: 'post',
+      statusCode: 200
     });
     expect(result.code).toBe(0);
   });
-  
-  it("user GET should be 200", async () => {
+
+  it('user GET should be 200', async () => {
     // test api reject
     try {
       await http({
         url,
-        method : "get",
-        statusCode : 400
+        method: 'get',
+        statusCode: 400
       });
     } catch (e) {
-      expect(e.msg).toBe("invald params");
+      expect(e.msg).toBe('invald params');
     }
   });
-  
 });
 
 // service cases
 describe('service initUserData test', () => {
-  
-  it("if user age is 18, age should be 19", async () => {
+  it('if user age is 18, age should be 19', async () => {
     expect.assertions(1);
+    util.useErrorMode = true;
     const result = await initUserData();
     expect(result.data.age).toBe(19);
+    util.useErrorMode = false;
   });
-  
 });
--- src/service/someNeedTest.js
+++ src/service/someNeedTest.js
@@ -1,20 +1,16 @@
 import getUser from '../api/user';
 
-const util = require('../util');
-
 export default async function initUserData() {
-  /* istanbul ignore next */
-  util.useErrorMode = true;
   try {
     const user = await getUser();
-    console.log(user);
     // console.log(user); // age 18
+    /* istanbul ignore next */
     if (user.data.age === 18) {
       user.data.age += 1;
     }
-    return user; // age 19
+    
+    return user;
   } catch (e) {
-    return 'error';
+    return e;
   }
-  util.useErrorMode = false; // Restore after finish test.
 }
